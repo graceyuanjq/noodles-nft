d8ee59be04143af253af303de4739c2eb2bc9cf7
Grace Yuan <73268280+graceyuanjq@users.noreply.github.com>
2022-01-15 21:09:59 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -26,7 +26,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 100,
+    growEditionSizeTo: 200,
     layersOrder: [
       { name: "Background" },
       { name: "Head" },
